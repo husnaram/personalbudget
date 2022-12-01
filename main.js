768bc6ff91223f745940e94543010fd4e5a61808
Husna Ramadan <husna.ramadan@gmail.com>
2022-12-01 16:57:17 +0700

    Add Joi validation

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import { v4 as uuidv4 } from 'uuid';
+import * as joi from "joi";
 
 dotenv.config();
 
@@ -9,6 +10,19 @@ const app = express();
 
 // Parsing application/json
 app.use(express.json())
+
+// Schema for create envelope validation
+const createEnvelopeSchema = joi.object({
+	title: joi
+		.string()
+		.min(3)
+		.max(20)
+		.required(),
+	budget: joi
+		.number()
+		.integer()
+		.required()
+});
 
 app.get("/", (req, res) => res.send("Hello, World"));
 app.post("/envelopes", (req, res) => {
